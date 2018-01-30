cc780fe8020574e07265b6c08b60603aa9118eee
gibl3t <developer.thorner@gmail.com>
2018-01-29 20:35:43 -0500

    Create Array2d

---
+++ array2d.py
@@ -0,0 +1,22 @@
+class Array2d:
+    
+    #constructor
+    def __init__(self, height, width):
+        self.rows = [None] * width
+        i = 0 
+        while i < len(self.rows):
+            self.column = [None] * height
+            self.rows[i] = self.column
+            i += 1
+
+    def get(self, x, y):
+        return self.rows[x][y]
+
+    def set(self, x, y, val):
+        self.rows[x][y] = val
+
+myArray = Array2d(3,3)
+#print(myArray.rows)
+
+myArray.set(1,1,3)
+print(myArray.get(1,1))
