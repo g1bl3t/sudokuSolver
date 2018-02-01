12e39fcb4788bd2cf8ead5ed53730b90ec7dbc8f
gibl3t <developer.thorner@gmail.com>
2018-02-01 16:02:37 -0500

    Finish checkRows and checkColumns

--- puzzle.py
+++ puzzle.py
@@ -34,30 +34,53 @@ class Puzzle:
     
     def checkRows(self):
         numsFound = [0 for zero in range(10)]
-        print("numsFound init={}".format(numsFound))
+        #print("numsFound init={}".format(numsFound))
         sum = 0
         
         #Check for dupes
         for x in range(9):
             for y in range(9):
                 thisNum = self.matrix.get(x,y)
-                print("x:{}y:{}".format(x,y))
-                print("thisNum = {}".format(thisNum))
+                #print("x:{}y:{}".format(x,y))
+                #print("thisNum = {}".format(thisNum))
                 if numsFound[thisNum] == 1:
-                    print("thisNum={}numsFound[]={}".format(thisNum,numsFound[thisNum]))
+                    #print("thisNum={}numsFound[]={}".format(thisNum,numsFound[thisNum]))
                     numsFound = [0 for zero in range(10)]
-                    print("numsFound again{}".format(numsFound))
+                    #print("numsFound again{}".format(numsFound))
                     return False
                 else:
                     numsFound[thisNum] = 1
                     sum += thisNum
-        numsFound = [0 for zero in range(10)]
-        #Check sum, solved = 45
-        if sum == 45:
-            return True
-        else:
-            return False
+        
+            numsFound = [0 for zero in range(10)]
+            #Check sum, solved = 45
+            if sum == 45:
+                return True
+            else:
+                return False
 
+    def checkColumns(self):
+        numsFound = [0 for zero in range(10)]
+        sum = 0
+
+        #check for dupes
+        for y in range(9):
+            for x in range(9):
+                thisNum = self.matrix.get(x,y)
+
+                if numsFound[thisNum] == 1:
+                    numsFound = [0 for zero in range(10)]
+                    return False
+                else:
+                    numsFound[thisNum] = 1
+                    sum += thisNum
+
+                numsFound = [0 for zero in range(10)]
+                #Check sum, solved == 45
+                if sum == 45:
+                    return True
+                else:
+                    return False
 
 #lst = [0,1,2,3,4,5,6,7,8]
 #puzz = Puzzle(lst)
@@ -65,3 +88,4 @@ testList = Puzzle.fileToList("testPuzz")
 print(testList)
 puzz = Puzzle(testList)
 print(puzz.checkRows())
+print(puzz.checkColumns())
